d71c5c45ae002c9b42977bdc8d14c20f898ee7f0
Łukasze <lukaszkucharzyk17@gmail.com>
2023-06-16 17:02:00 +0200

    added rollable submenu in mobile menu

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ class Navigation{
         submenuListeners.forEach( listener => listener.addEventListener('pointerdown', this.handleMobileMainMenu));
     }
     handleMobileMainMenu(e){
-        console.log(e.currentTarget);
+        
+       const highLightedEl = e.currentTarget.querySelector('div:first-child');
+        highLightedEl.style.backgroundColor = '#ADE1F5';
+        setTimeout(() => {
+            highLightedEl.style.backgroundColor= 'transparent';
+        }, 100);
+
        const submenu = e.currentTarget.querySelector('.submenu');
-       console.log(submenu);
-       
-       if(submenu.classList.contains('rollable')){
-            submenu.classList.toggle('rolled')
+       if(submenu.classList.contains('rollable') || submenu.classList.contains('rolled')){
+            submenu.classList.toggle('rolled');
+            submenu.classList.toggle('rollable');
         }else{
             submenu.classList.toggle('hidden');
         } 
